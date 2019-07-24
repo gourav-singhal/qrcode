a65a8afffca7496670d8babc13bad6ef2d5eee96
Sergey <gaykov@users.noreply.github.com>
2019-07-24 12:58:20 +0300

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,5 +1,11 @@
-if [ -f "$destdir" ]
+GOOGLE_SERVICES_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/android/app/google-services.json
+
+if [ -e "$GOOGLE_SERVICES_JSON_FILE" ]
 then
-    echo "Overwriting google-services.json file..."
-    echo "$GOOGLE-SERVICES-JSON" > "android/app/google-services.json"
+    echo "Updating Google Json"
+    echo "$GOOGLE_SERVICES_JSON" > $GOOGLE_SERVICES_JSON_FILE
+    sed -i -e 's/\\"/'\"'/g' $GOOGLE_SERVICES_JSON_FILE
+
+    echo "File content:"
+    cat $GOOGLE_SERVICES_JSON_FILE
 fi
